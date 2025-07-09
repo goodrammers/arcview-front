a6521865ea4010d741a0434fdac7cbe95d16067e
Kwon <kwon@goodrammers.com>
2025-07-09 22:50:49 +0900

    메뉴로 비디오 선택 하기 추가

--- src/composables/Video.ts
+++ src/composables/Video.ts
@@ -1,11 +1,17 @@
 import { onBeforeMount, onMounted, ref } from 'vue'
 
-export function useVideo() {
+// video 를 가져다 쓰는 곳에서 직접  requestVideoFrameCallback 를 호출하여 사용하는 경우,
+// 정지된 상태에서는   requestVideoFrameCallback 에 의해 호출되지 않는 경우가 발생할 수 있음
+// 따라서, video 를 숨겨진 캔버스에 일단 캡쳐 해놓고, video 데이터가 필요한 경우 캡쳐된 캔버스를 사용한다.
+export function useVideo(canvasToCapture: HTMLCanvasElement | null = null) {
     const videoEl = ref<HTMLVideoElement | null>(null)
-    const isPlaying = ref(false)
     const duration = ref(0)
     const currentTime = ref(0)
+    const captured = ref(0)
     let onEndedCallback: null | (() => void) = null
+
+    let videoWidth = 0
+    let videoHeight = 0
 
     function initValue() {
         const video = videoEl.value
@@ -14,9 +20,18 @@ export function useVideo() {
         }
         duration.value = video.duration
 
-        const w = video.videoWidth
-        const h = video.videoHeight
-        video.style.aspectRatio = `${w} / ${h}`
+        videoWidth = video.videoWidth
+        videoHeight = video.videoHeight
+        video.style.aspectRatio = `${videoWidth} / ${videoHeight}`
+
+        if (canvasToCapture) {
+            // canvas 의 백스토어 사이즈를 video 의 사이즈와 맞추기
+            const dpr = window.devicePixelRatio || 1
+            canvasToCapture.width = videoWidth * dpr
+            canvasToCapture.height = videoHeight * dpr
+            const ctx = canvasToCapture.getContext('2d')
+            ctx!.scale(dpr, dpr)
+        }
     }
 
     function setOnEndedCallback(cb: () => void) {
@@ -24,7 +39,6 @@ export function useVideo() {
     }
 
     function onEnded() {
-        isPlaying.value = false
         if (onEndedCallback) {
             onEndedCallback()
         }
@@ -34,11 +48,33 @@ export function useVideo() {
         currentTime.value = videoEl.value!.currentTime
     }
 
+    function captureFrame() {
+        if (!canvasToCapture) {
+            return
+        }
+        const video = videoEl.value
+        if (!video) {
+            return
+        }
+        function loop() {
+            const ctx = canvasToCapture!.getContext('2d')
+            ctx!.drawImage(video!, 0, 0, videoWidth, videoHeight)
+            captured.value++
+            video!.requestVideoFrameCallback(loop)
+        }
+        video.requestVideoFrameCallback(loop)
+    }
+
+    function getResolution() {
+        return { width: videoWidth, height: videoHeight }
+    }
+
     onMounted(() => {
         if (videoEl.value) {
             videoEl.value.addEventListener('loadedmetadata', initValue)
             videoEl.value.addEventListener('ended', onEnded)
             videoEl.value.addEventListener('timeupdate', updateCurrentTime)
+            captureFrame()
         }
     })
     onBeforeMount(() => {
@@ -53,6 +89,9 @@ export function useVideo() {
         videoEl,
         currentTime,
         duration,
+        captured,
+
         setOnEndedCallback,
+        getResolution,
     }
 }
