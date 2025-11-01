523813185bb54c2e911fee95ed2b364a6c68f087
Kwon <kwon@goodrammers.com>
2025-11-01 21:11:54 +0900

    작업 목록 UI 변경

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHashHistory, type RouteRecordRaw } from 'vue-router'
 import WeldingView from '@/pages/WeldingView.vue'
-import Login from '@/pages/Login.vue'
 import Dashboard from '@/pages/Dashboard.vue'
 import MainLayout from '@/layout/MainLayout.vue'
 import RealTime from '@/pages/RealTime.vue'
